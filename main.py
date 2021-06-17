0e6f4a12b0f8561f6440e1e3503462944af05a56
Kasania <nasang1997@gmail.com>
2021-06-17 17:03:27 +0900

    Update \!rr command accept bonus and penalty

--- main.py
+++ main.py
@@ -27,6 +27,8 @@ if __name__ == '__main__':
         "이성": "V15"
     }
 
+
+
 @app.event
 async def on_ready():
     print(f'{app.user} has connected to Discord!')
@@ -45,7 +47,6 @@ async def help_message(ctx: discord.ext.commands.Context):
                                   '`!roll NdR + X` `!roll NdR - X` `!roll NdR * X` `!roll NdR / X`\n'
                                   '`!roll NdR < X` `!roll NdR > X` `!roll NdR <= X` `!roll NdR >= X`\n'
                                   '`!roll NdR + MdS ...`\n'
-                                  '`!roll ccc`\n'
                                   '\n'
                                   '※ `!r NdR`로 입력하실 수도 있습니다.\n'
                                   '\n'
@@ -58,19 +59,43 @@ async def help_message(ctx: discord.ext.commands.Context):
                                   '\n'
                                   '`!r 1d100 < 50`처럼 판정값의 성공/실패를 체크할 수 있습니다.\n'
                                   '\n'
-                                  '`!r ccc`로 CoC 7th 캐릭터 메이킹을 빠르게 진행 할 수 있습니다.'
-                                  '\n\n'
-                                  '(테스트 중인 기능!)\n'
-                                  '`!ruse [구글스프레드시트링크] [시트이름]`으로 간편 판정 탐사자를 등록 할 수 있습니다.'
-                                  'ex> `!ruse https://docs.google.com/spreadsheets/d'
-                                  '/1ZzXjzplwt3lSnNNsrnQfrPtmUsHvEfgxK31XWEa2lWA \"조 종사(Niq)\"` '
+                                  '`!다이스2`로 CoC 7th 특화기능 명령어를 확인 하실 수 있습니다.',
+                                  color=0xff8400)
+    await ctx.send(embed=embed_message)
+
+
+@app.command(name='다이스2')
+async def help_message2(ctx: discord.ext.commands.Context):
+    embed_message = discord.Embed(title=f":game_die: 주사위(다이스) 사용법",
+                                  description=
+                                  '**CoC 7th 특화기능**\n'
+                                  '`!roll ccc`\n'
+                                  '`!ruse [시트링크] [시트이름]`\n'
+                                  '`!rr [특성치/기능치] (보정치)`\n'
+                                  '`!rstat`\n'
+                                  '`!rreset`\n'
+                                  '`!rclear`\n'
+                                  '\n'
+                                  '`!r ccc`로 CoC 7th 캐릭터 메이킹을 빠르게 진행 할 수 있습니다.\n'
+                                  '`!ruse [구글스프레드시트링크] [시트이름]`으로 간편 판정 탐사자를 등록 할 수 있습니다.\n'
+                                  'ex> `!ruse https://docs.google.com/spreadsheets/d/1CzAo97L-ioGFHo_d8MC64nxAKiLchd'
+                                  '-MkixYL4mxjwE \"조 종사(Niq)\"` '
+                                  '\n'
                                   '\n'
                                   '`!rr [판정이름]`으로 등록한 탐사자 특성/기능치 판별을 빠르게 할 수 있습니다.'
-                                  'ex> `!rr 심리학`'
+                                  '\n'
+                                  '`!rr [판정이름] [일반표현식]`으로 보정치를 설정 할 수 있습니다. **(띄어쓰기 필수!)**'
+                                  '\n'
+                                  '`!rr [판정이름] b[개수]` `!rr [판정이름] p[개수]`으로 보너스/페널티 주사위를 사용 할 수 있습니다. **(띄어쓰기 필수!)**'
+                                  '\n'
+                                  'ex> `!rr 심리학` `!rr 심리학 +20` `!rr 심리학 -((4d6)/5)` `!rr 심리학 b4` `!rr 심리학 p3` '
+                                  '\n'
                                   '\n'
                                   '`!rstat`으로 등록한 탐사자 이름과 시트 링크를 확인 할 수 있습니다.'
                                   '\n'
+                                  '\n'
                                   '`!rreset`으로 등록한 탐사자를 해제할 수 있습니다.'
+                                  '\n'
                                   '\n'
                                   '`TRPG 마스터는 !rclear`으로 등록된 모든 탐사자를 일괄 해제할 수 있습니다.'
                                   ,
@@ -95,9 +120,7 @@ async def roll(ctx: discord.ext.commands.Context, *args):
 
 # ----
 
-
-def roll_sheet(worksheet: gspread.Worksheet, target):
-    value = sd.dice(1, 100, [])
+def get_sheet_value(worksheet: gspread.Worksheet, target):
     if target in fixed_sheet_position.keys():
         cell = worksheet.acell(fixed_sheet_position[target])
         v = int(cell.value)
@@ -106,7 +129,7 @@ def roll_sheet(worksheet: gspread.Worksheet, target):
         row = cell.row
         col = cell.col
         v = int(worksheet.cell(row, col + 4).value)
-    return [value, v, judgement(value, v)]
+    return v
 
 
 def judgement(value, v):
@@ -119,6 +142,61 @@ def judgement(value, v):
         value == 1: "1!",
         value == 100: "100!"
     }.get(True)
+    return [value, result]
+
+
+# roll nd10
+# pickup min num,
+# return value mod10 + min*10
+def calc_bonus(value, num, dices):
+    dice = list()
+    sd.dice(num, 10, dice)
+    d = dice[0][:-1]
+    d.sort()
+    v = value % 10
+    if v == 0:
+        if value // 10 > d[0]:
+            result = v + d[0] * 10
+        else:
+            result = value
+    else:
+        if 10 in d:
+            result = v
+        else:
+            if value // 10 > d[0]:
+                result = v + d[0] * 10
+            else:
+                result = value
+    d = [x if x != 10 else 0 for x in d]
+    d.sort()
+    dices.append([x * 10 for x in d])
+    return result
+
+
+def calc_penalty(value, num, dices):
+    dice = list()
+    sd.dice(num, 10, dice)
+    d = dice[0][:-1]
+    d.sort()
+    v = value % 10
+    if v == 0:
+        if 10 in d:
+            result = 100
+        else:
+            d = [x if x != 10 else 0 for x in d]
+            d.sort()
+            if value // 10 < d[-1]:
+                result = v + d[-1] * 10
+            else:
+                result = value
+    else:
+        d = [x if x != 10 else 0 for x in d]
+        d.sort()
+        if value // 10 < d[-1]:
+            result = v + d[-1] * 10
+        else:
+            result = value
+    dices.append([x * 10 for x in d])
     return result
 
 
@@ -127,22 +205,52 @@ def judgement(value, v):
 # !rr (근력 condition)
 # !rr 근력 +(d10+20)
 # => replace "sheet-value" to scalar value
-# !rr 근력 p2
-# !rr 근력 b1
+
+
+# !rr 근력 p2 -> correct
+# !rr 근력 b1 -> correct
+
+# find value from sheet -> calc expression -> judgement values -> return result
 
 @app.command(name='rr', pass_context=True)
 async def roll2(ctx: discord.ext.commands.Context, *args):
     key = str(ctx.guild) + str(ctx.author)
-    if key in player:
-        sheet = player[key]
-        result = roll_sheet(sheet, args[0])
-        embed_message = discord.Embed(title=f":game_die: {args[0]} 판정 : {result[2]}!", color=0xff8400)
-        embed_message.add_field(name="플레이어", value=ctx.message.author.mention, inline=False)
-        embed_message.add_field(name="판정값", value=f"{result[0]} <= {result[1]}", inline=False)
-        await ctx.send(embed=embed_message)
-    else:
-        embed_message = discord.Embed(title=f"등록된 탐사자가 없습니다.", description=f"플레이어 : {ctx.author.mention}",
+    try:
+
+        if key in player:
+            sheet = player[key]
+            sheet_value = get_sheet_value(sheet, args[0])
+            dice_value = sd.dice(1, 100, [])
+            dices = [dice_value]
+            if len(args) > 1:
+                if str(args[1]).startswith('b'):
+                    expr = sd.calc_expr(''.join(args[1:])[1:], dices, [])
+                    dice_value = calc_bonus(dice_value, expr, dices)
+                elif str(args[1]).startswith('p'):
+                    expr = sd.calc_expr(''.join(args[1:])[1:], dices, [])
+                    dice_value = calc_penalty(dice_value, expr, dices)
+                else:
+                    expr = str(dice_value) + ''.join(args[1:])
+                    dice_value = sd.calc_expr(expr, dices, [])
+            jud = judgement(dice_value, sheet_value)
+            embed_message = discord.Embed(title=f":game_die: {args[0]} 판정 : {jud[1]}!", color=0xff8400)
+            embed_message.add_field(name="플레이어", value=ctx.message.author.mention, inline=False)
+            embed_message.add_field(name="판정값", value=f"{jud[0]} <= {sheet_value}", inline=False)
+
+            if len(dices) > 1:
+                embed_message.add_field(name="주사위 목록",
+                                        value='\n'.join(map(str, dices)), inline=False)
+
+            await ctx.send(embed=embed_message)
+        else:
+            embed_message = discord.Embed(title=f"등록된 탐사자가 없습니다.", description=f"플레이어 : {ctx.author.mention}",
+                                          color=0xff8400)
+            await ctx.send(embed=embed_message)
+
+    except Exception as e:
+        embed_message = discord.Embed(title=f":x: 지원하지 않는 명령어입니다.", description=f"플레이어 : {ctx.author.mention}",
                                       color=0xff8400)
+        print(e)
         await ctx.send(embed=embed_message)
 
 
@@ -165,14 +273,17 @@ async def use_sheet(ctx: discord.ext.commands.Context, *args):
     except gspread.exceptions.APIError as e:
         err_text = str(e.response)
         if err_text == '<Response [403]>':
-            embed_message = discord.Embed(title=f":x: 시트의 접근권한이 없습니다.", description='시트의 접근권한을 변경해 주세요.',
+            embed_message = discord.Embed(title=f":x: 시트의 접근권한이 없습니다.",
+                                          description=f"플레이어 : {ctx.author.mention} \n 시트의 접근권한을 변경해 주세요.",
                                           color=0xff8400)
             await ctx.send(embed=embed_message)
     except gspread.exceptions.WorksheetNotFound:
         embed_message = discord.Embed(title=":x: 올바르지 않은 시트이름입니다.", color=0xff8400)
         await ctx.send(embed=embed_message)
     except Exception as e:
-        embed_message = discord.Embed(title=f":x: 명령을 수행하는데 실패했습니다.", description=str(e), color=0xff8400)
+        embed_message = discord.Embed(title=f":x: 명령을 수행하는데 실패했습니다.",
+                                      description=f"플레이어 : {ctx.author.mention}", color=0xff8400)
+        print(e)
         await ctx.send(embed=embed_message)
 
 
@@ -190,7 +301,9 @@ async def add_sheet(ctx: discord.ext.commands.Context):
                                           color=0xff8400)
             await ctx.send(embed=embed_message)
     except Exception as e:
-        embed_message = discord.Embed(title=f":x: 명령을 수행하는데 실패했습니다.", description=str(e), color=0xff8400)
+        embed_message = discord.Embed(title=f":x: 명령을 수행하는데 실패했습니다.",
+                                      description=f"플레이어 : {ctx.author.mention}", color=0xff8400)
+        print(e)
         await ctx.send(embed=embed_message)
 
 
@@ -209,7 +322,9 @@ async def stat_sheet(ctx: discord.ext.commands.Context):
                                           color=0xff8400)
             await ctx.send(embed=embed_message)
     except Exception as e:
-        embed_message = discord.Embed(title=f":x: 명령을 수행하는데 실패했습니다.", description=str(e), color=0xff8400)
+        embed_message = discord.Embed(title=f":x: 명령을 수행하는데 실패했습니다.",
+                                      description=f"플레이어 : {ctx.author.mention}", color=0xff8400)
+        print(e)
         await ctx.send(embed=embed_message)
 
 
@@ -237,8 +352,9 @@ async def stat_sheet(ctx: discord.ext.commands.Context):
                                           description=f"마스터: {ctx.author.mention}",
                                           color=0xff8400)
     else:
-        embed_message = discord.Embed(title=f":x: 명령을 수행할 권한이 없습니다.", color=0xff8400)
+        embed_message = discord.Embed(title=f":x: 명령을 수행할 권한이 없습니다.",
+                                      description=f"플레이어 : {ctx.author.mention}", color=0xff8400)
     await ctx.send(embed=embed_message)
 
-    
 app.run(keys.TOKEN)
+
